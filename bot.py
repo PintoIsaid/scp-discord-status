24767261122c51f99fa99da01c07cbb724fb44fa
PintoIsaid <isaidpinto29@gmail.com>
2026-05-31 21:24:25 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,14 +12,29 @@ client = discord.Client(intents=intents)
 @tasks.loop(minutes=2)
 async def update_channel():
     try:
-        players = "player"
-        online = True
-        guild = client.guilds[0]
-        channel = guild.get_channel(CHANNEL_ID)
+        async with aiohttp.ClientSession() as session:
+        async with session.get("https://api.scpslgame.com/lobbylist.php?format=json") as resp:
+            data = await resp.json()
 
-        if channel:
-            status = "🟢 Online" if online else "🔴 Offline"
-            await channel.edit(name=f"{status} | {players}")
+    players = "0/60"
+    online = False
+
+    for server in data:
+        if "UNDERGROUND-PROJECT" in str(server):
+            online = True
+
+            current = server.get("players", 0)
+            maxplayers = server.get("maxplayers", 60)
+
+            players = f"{current}/{maxplayers}"
+            break
+
+    guild = client.guilds[0]
+    channel = guild.get_channel(CHANNEL_ID)
+
+    if channel:
+        status = "🟢 Online" if online else "🔴 Offline"
+        await channel.edit(name=f"{status} | {players}")
 
     except Exception as e:
         print(e)
